a3d943c6b53ec36629eb60a3a4d6a8d056c09247
cmd05 <splendid.snippet670@slmail.me>
2024-09-29 13:24:16 +0530

    add systick

---
+++ 11.systick_delay/Core/Src/main.c
@@ -0,0 +1,37 @@
+#include "stm32f4xx.h"
+#include <stdint.h>
+#include <stdio.h>
+
+#include "systick.h"
+#include "uart.h"
+#include "adc.h"
+
+uint32_t sensor_value;
+
+#define GPIOAEN		(1U << 0)
+#define GPIOA_5		(1U << 5)
+
+#define LED_PIN		GPIOA_5
+
+// Single channel - single conversion mode
+int main() {
+	// LED initialization
+	RCC->AHB1ENR |= GPIOAEN;
+
+	GPIOA->MODER |= (1U << 10);
+	GPIOA->MODER &= ~(1U << 11);
+
+	// write data to serial
+	uart2_tx_init();
+
+	while(1) {
+		printf("A second passed\n\r"); // sent from board to computer
+//		printf("SysTick %u\n\r", SysTick->VAL); // sent from board to computer
+		GPIOA->ODR ^= LED_PIN;
+		systickDelayMs(1000); // 1 second
+	}
+}
+
+
+
+
