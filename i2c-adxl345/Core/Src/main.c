0d72c633a4ef98b5b08ebd56d2271e7d9b976d41
cmd05 <splendid.snippet670@slmail.me>
2025-01-03 15:55:44 +0530

    enable adxl. change scale to `float`

--- i2c-adxl345/Core/Src/main.c
+++ i2c-adxl345/Core/Src/main.c
@@ -4,7 +4,7 @@
 #include "uart.h"
 
 extern uint8_t g_data_rec[6];
-const int FOUR_G_SCALE = 0.0078;
+const float FOUR_G_SCALE = 0.0078;
 
 double x, y, z;
 double xg, yg, zg;
@@ -16,18 +16,18 @@ int main() {
     I2C1_init();
     I2C1_scan_bus();
 
-    // adxl_init();
+    adxl_init();
 
     while(1) {
-        // adxl_read_values(DATA_START_ADDR);
+        adxl_read_values(DATA_START_ADDR);
 
-        // x = (g_data_rec[1] << 8) | g_data_rec[0];
-        // y = (g_data_rec[3] << 8) | g_data_rec[2];
-        // z = (g_data_rec[5] << 8) | g_data_rec[4];
+        x = (g_data_rec[1] << 8) | g_data_rec[0];
+        y = (g_data_rec[3] << 8) | g_data_rec[2];
+        z = (g_data_rec[5] << 8) | g_data_rec[4];
 
-        // xg = (x * FOUR_G_SCALE);
-        // yg = (y * FOUR_G_SCALE);
-        // zg = (z * FOUR_G_SCALE);
+        xg = (x * FOUR_G_SCALE);
+        yg = (y * FOUR_G_SCALE);
+        zg = (z * FOUR_G_SCALE);
     }
 }
 
